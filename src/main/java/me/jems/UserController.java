f5f817feb2cec9ccfc39f6886c0caeddeb658a61
sraminics <sindhujaramini@gmail.com>
2019-05-05 23:59:44 -0700

    Updated User API to reflect status codes

--- src/main/java/me/jems/UserController.java
+++ src/main/java/me/jems/UserController.java
@@ -1,10 +1,13 @@
 package me.jems;
 
-import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -16,29 +19,37 @@ public class UserController extends AppController {
     UserRepository userRepository;
 
     @PostMapping("/createuser")
-    public Users createUser(@RequestBody Map<String, String> body){
+    public ResponseEntity<Users> createUser(@RequestBody Map<String, String> body){
        String username = body.get("username");
        String hashedPassword = body.get("hashedPassword");
-       return userRepository.save(new Users(username, hashedPassword));
+       if(username != null && hashedPassword != null) {
+           Users user = new Users(username, hashedPassword);
+           userRepository.save(user);
+           HttpHeaders responseHeaders = new HttpHeaders();
+           return new ResponseEntity<>(user, responseHeaders, HttpStatus.OK);
+       }
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/authenticate")
-    public Boolean authenticate(@RequestBody Map<String, String> body) {
+    public ResponseEntity<Users> authenticate(@RequestBody Map<String, String> body) {
         session_id = false;
         if(!body.isEmpty()) {
             String username = body.get("username");
             String hashedPassword = body.get("hashedPassword");
-            ArrayList<Users> users = (ArrayList<Users>) userRepository.findByUserNameEquals(username);
-            for(Users u: users) {
-                if(u.getHashedPassword().equals(hashedPassword)) {
-                    session_id = true;
-                    return true;
+            if(username != null && hashedPassword != null) {
+                ArrayList<Users> users = (ArrayList<Users>) userRepository.findByUserNameEquals(username);
+                for(Users u: users) {
+                    if(u.getHashedPassword().equals(hashedPassword)) {
+                        session_id = true;
+                        return new ResponseEntity<>(u, HttpStatus.OK);
+                    }
                 }
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
+
         }
-
-        return session_id;
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
 
 }
